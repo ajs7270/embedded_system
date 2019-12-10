42c23a04c9e75cf9a74dddc481507adda9c56d13
jisuAn <ajs7270@naver.com>
2019-12-10 00:48:33 -0500

    modify push_switch driver

--- push_switch/push_switch.c
+++ push_switch/push_switch.c
@@ -6,6 +6,7 @@
 #include <linux/platform_device.h>
 #include <linux/delay.h>
 
+#include <linux/miscdevice.h>	// needed by misc device driver function
 #include <asm/io.h>
 #include <asm/uaccess.h>
 #include <linux/kernel.h>
@@ -17,8 +18,8 @@
 
 #define MAX_BUTTON 9
 
-#define IOM_FPGA_PUSH_SWITCH_MAJOR 265				// ioboard led device major number
-#define IOM_FPGA_PUSH_SWITCH_NAME "fpga_push_switch"	// ioboard led device name
+#define IOM_FPGA_PUSH_SWITCH_MAJOR 265			
+#define IOM_FPGA_PUSH_SWITCH_NAME "fpga_push_switch"
 
 #define IOM_FPGA_PUSH_SWITCH_ADDRESS 0x050			// pysical address
 extern ssize_t iom_fpga_itf_read(unsigned int addr);
@@ -31,15 +32,6 @@ static unsigned char *iom_demo_addr;
 ssize_t iom_fpga_push_switch_read(struct file *inode, char *gdata, size_t length, loff_t *off_what); 
 int iom_fpga_push_switch_open(struct inode *minode, struct file *mfile);
 int iom_fpga_push_switch_release(struct inode *minode, struct file *mfile);
-
-struct file_operations iom_fpga_push_switch_fops =
-{
-	owner:		THIS_MODULE,
-	open:		iom_fpga_push_switch_open,
-	read:		iom_fpga_push_switch_read,	
-	release:	iom_fpga_push_switch_release,
-};
-
 int iom_fpga_push_switch_open(struct inode *minode, struct file *mfile) 
 {	
 	if(fpga_push_switch_port_usage != 0) return -EBUSY;
@@ -77,22 +69,27 @@ ssize_t iom_fpga_push_switch_read(struct file *inode, char *gdata, size_t length
 	return length;	
 }
 
+struct file_operations iom_fpga_push_switch_fops =
+{
+	owner:		THIS_MODULE,
+	open:		iom_fpga_push_switch_open,
+	read:		iom_fpga_push_switch_read,	
+	release:	iom_fpga_push_switch_release,
+};
+
+static struct miscdevice push_switch_driver = {
+	.fops	= &iom_fpga_push_switch_fops,
+	.name	= IOM_FPGA_PUSH_SWITCH_NAME,
+	.minor	= MISC_DYNAMIC_MINOR,
+};
+
+
 int __init iom_fpga_push_switch_init(void)
 {
-	int result;
-
-	result = register_chrdev(IOM_FPGA_PUSH_SWITCH_MAJOR, IOM_FPGA_PUSH_SWITCH_NAME, &iom_fpga_push_switch_fops);
-	if(result < 0) {
-		printk(KERN_WARNING"Can't get any major\n");
-		return result;
-	}
-
-	//iom_fpga_push_switch_addr = ioremap(IOM_FPGA_PUSH_SWITCH_ADDRESS, 0x18);
-
-	printk("init module, %s major number : %d\n", IOM_FPGA_PUSH_SWITCH_NAME, IOM_FPGA_PUSH_SWITCH_MAJOR);
-
+	misc_register(&push_switch_driver);
 	return 0;
 }
+
 
 void __exit iom_fpga_push_switch_exit(void) 
 {
